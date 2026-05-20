4c5f33dfec9925cf24f972f6104d01f9033adcc5
seungmin <ssmmkim1025@naver.com>
2026-05-21 01:54:18 +0900

    :art: Design: footer 이미지로 수정

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,12 @@
 import styled from "styled-components";
 import iconurl from '../../assets/icons/skon.png';
+import imageurl from "../../assets/images/footer.png";
 
 export default function Footer(){
     return (
         <Wrapper>
             <FooterContainer>
-                <SkuText> SKULIKELION.</SkuText>
-                <SkuText2> ALL RIGHTS RESERVED.</SkuText2>
-                <SkuText3> Seokyeong University, 124 Seokyeong-ro, Seongbuk-gu, Seoul, 02726, Republic of Korea</SkuText3>
+                <Image src ={imageurl}/>
                 <IconBox>
                     <Icon src={iconurl} />
                 </IconBox>
@@ -37,30 +36,10 @@ const IconBox = styled.div`
     display: flex;
     justify-content: flex-end;
 `;
-const SkuText = styled.p`
-    color: #00156A;
-    font-size: 24px;
-    font-family: New Font;
-    font-weight: 400;
-    word-wrap: break-word
-`;
- const SkuText2 = styled.p`
-    color: #3C3C3C;
-    font-size: 14px;
-    font-family: New Font;
-    font-weight: 400;
-    letter-spacing: 0.50px;
-    word-wrap: break-word
-`;
-const SkuText3 = styled.p`
-    color: #686868;
-    font-size: 11px;
-    font-family: New Font;
-    font-weight: 400;
-    line-height: 28px;
-    letter-spacing: 0.50px;
-    word-wrap: break-word;
-`;
+const Image = styled.img`
+    width: 635px;
+;`
+
 
 
     
